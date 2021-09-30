75a6f91f5a961fe0d34bfc9ce4e1a2992be0e2f8
FernandoNobel <fersann1@upv.es>
2021-09-30 16:48:53 +0200

    Add mass fix

--- build/multiscale.m
+++ build/multiscale.m
@@ -41,8 +41,6 @@ classdef multiscale
 			p.cell__nu_max = 1260.0;
 			p.cell__m_aa = 1.826e-07;
 			p.cell__phi_t = 0.9473;
-			p.mass__mp_mp0 = 773.748;
-			p.mass__mp_beta = 61.7813;
 		end
 
 		function x0 = initial_conditions(~)
@@ -92,24 +90,22 @@ classdef multiscale
 			cell__r = x(5,:);
 
 			% Assigment states:
+			cell__m_h = 450;
 			cell__p_r__mu = cell__mu;
 			cell__p_r__r = cell__r;
+			cell__p_r__m_h = cell__m_h;
 			cell__p_r__E_m = 3.459;
 			cell__p_r__r_t = cell__p_r__m./p.cell__p_r__weigth;
 			cell__p_nr__mu = cell__mu;
 			cell__p_nr__r = cell__r;
+			cell__p_nr__m_h = cell__m_h;
 			cell__p_nr__E_m = 6.3492;
 			cell__nu = p.cell__nu_max;
 			cell__m_p = cell__p_r__m + cell__p_nr__m;
-			mass__mu = cell__mu;
-			mass__m_h = p.mass__mp_mp0.*exp(p.mass__mp_beta.*mass__mu);
-			cell__m_h = mass__m_h;
 			cell__p_r__nu = cell__nu;
-			cell__p_r__m_h = cell__m_h;
 			cell__p_r__K_C0 = p.cell__p_r__k_b./(p.cell__p_r__k_u + cell__p_r__nu./p.cell__p_r__l_e);
 			cell__p_r__J = cell__p_r__E_m.*p.cell__p_r__omega./(p.cell__p_r__d_m./cell__p_r__K_C0 + cell__p_r__mu.*cell__p_r__r);
 			cell__p_nr__nu = cell__nu;
-			cell__p_nr__m_h = cell__m_h;
 			cell__p_nr__K_C0 = p.cell__p_nr__k_b./(p.cell__p_nr__k_u + cell__p_nr__nu./p.cell__p_nr__l_e);
 			cell__p_nr__J = cell__p_nr__E_m.*p.cell__p_nr__omega./(p.cell__p_nr__d_m./cell__p_nr__K_C0 + cell__p_nr__mu.*cell__p_nr__r);
 			cell__J_sum = p.cell__p_r__N.*cell__p_r__J + p.cell__p_nr__N.*cell__p_nr__J;
@@ -148,24 +144,22 @@ classdef multiscale
 			cell__r = x(5,:);
 
 			% Assigment states:
+			cell__m_h = 450;
 			cell__p_r__mu = cell__mu;
 			cell__p_r__r = cell__r;
+			cell__p_r__m_h = cell__m_h;
 			cell__p_r__E_m = 3.459;
 			cell__p_r__r_t = cell__p_r__m./p.cell__p_r__weigth;
 			cell__p_nr__mu = cell__mu;
 			cell__p_nr__r = cell__r;
+			cell__p_nr__m_h = cell__m_h;
 			cell__p_nr__E_m = 6.3492;
 			cell__nu = p.cell__nu_max;
 			cell__m_p = cell__p_r__m + cell__p_nr__m;
-			mass__mu = cell__mu;
-			mass__m_h = p.mass__mp_mp0.*exp(p.mass__mp_beta.*mass__mu);
-			cell__m_h = mass__m_h;
 			cell__p_r__nu = cell__nu;
-			cell__p_r__m_h = cell__m_h;
 			cell__p_r__K_C0 = p.cell__p_r__k_b./(p.cell__p_r__k_u + cell__p_r__nu./p.cell__p_r__l_e);
 			cell__p_r__J = cell__p_r__E_m.*p.cell__p_r__omega./(p.cell__p_r__d_m./cell__p_r__K_C0 + cell__p_r__mu.*cell__p_r__r);
 			cell__p_nr__nu = cell__nu;
-			cell__p_nr__m_h = cell__m_h;
 			cell__p_nr__K_C0 = p.cell__p_nr__k_b./(p.cell__p_nr__k_u + cell__p_nr__nu./p.cell__p_nr__l_e);
 			cell__p_nr__J = cell__p_nr__E_m.*p.cell__p_nr__omega./(p.cell__p_nr__d_m./cell__p_nr__K_C0 + cell__p_nr__mu.*cell__p_nr__r);
 			cell__J_sum = p.cell__p_r__N.*cell__p_r__J + p.cell__p_nr__N.*cell__p_nr__J;
@@ -212,8 +206,6 @@ classdef multiscale
 			out.cell__J_sum_E = cell__J_sum_E.*ones_t;
 			out.cell__phi_b_t = cell__phi_b_t.*ones_t;
 			out.cell__r = cell__r.*ones_t;
-			out.mass__mu = mass__mu.*ones_t;
-			out.mass__m_h = mass__m_h.*ones_t;
 
 			% Save parameters.
 			out.cell__p_r__N = p.cell__p_r__N.*ones_t;
@@ -235,8 +227,6 @@ classdef multiscale
 			out.cell__nu_max = p.cell__nu_max.*ones_t;
 			out.cell__m_aa = p.cell__m_aa.*ones_t;
 			out.cell__phi_t = p.cell__phi_t.*ones_t;
-			out.mass__mp_mp0 = p.mass__mp_mp0.*ones_t;
-			out.mass__mp_beta = p.mass__mp_beta.*ones_t;
 
 		end
 		function plot(~,out)
@@ -415,19 +405,6 @@ classdef multiscale
 			subplot(3,3,9);
 			plot(out.t, out.cell__p_nr__m);
 			title("cell__p_nr__m");
-			ylim([0, +inf]);
-			grid on;
-
-			figure('Name','mass');
-			subplot(2,1,1);
-			plot(out.t, out.mass__mu);
-			title("mass__mu");
-			ylim([0, +inf]);
-			grid on;
-
-			subplot(2,1,2);
-			plot(out.t, out.mass__m_h);
-			title("mass__m_h");
 			ylim([0, +inf]);
 			grid on;
 
