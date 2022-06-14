e969212a0fba00bc62d2d1adea7a33269337d079
FernandoNobel <fersann1@upv.es>
2022-06-14 18:58:37 +0200

    Done example_02.m

--- example_02.m
+++ example_02.m
@@ -3,8 +3,8 @@
 % Any changes you make to it will be overwritten the next time
 % the file is generated.
 
-% clear all;
-% close all;
+clear all;
+close all;
 
 % Init model.
 m = host_aware_antithetic_controller();
@@ -21,6 +21,42 @@ p.cell__z2__omega_max = 1;
 p.cell__x__omega_max = 1;
 p.cell__x__h = 0.001;
 
+% Simulation neglecting the burden produced to the host.
+p.cell____burden = 0;
+out_no_host = simulate(m, opt, p);
+
+% Simulation considering the burden produced to the host.
+p.cell____burden = 1;
+out_host = simulate(m, opt, p);
+
+
+%% Plot results
+
+fontSize = 8;
+axisSize = 6;
+
+f = figure('Position',[0,0,235,225]);
+set(gca,'Fontsize',axisSize);
+
+yyaxis left;
+hold on;
+ylabel('Protein $x$ $\; [$fg$]$','interpreter','latex','FontSize',fontSize);
+ylim([0 100]);
+plot(out_no_host.t, out_no_host.cell__x__m);
+plot(out_host.t, out_host.cell__x__m);
+
+yyaxis right;
+hold on;
+ylabel('Growth rate $\; [$min$^{-1}]$','interpreter','latex','FontSize',fontSize);
+ylim([0 0.03]);
+plot(out_no_host.t, out_no_host.cell__mu);
+plot(out_host.t, out_host.cell__mu);
+xlabel('Time $\; [h]$','interpreter','latex','FontSize',fontSize);
+
+
+%% Simulation function
+
+function [out] = simulate(m, opt, p)
 % Parameters for steady state first simulation.
 p_0 = p;
 p_0.tspan = [0 100000];
@@ -38,7 +74,7 @@ p_1.cell__z2__omega = 0;
 p_2 = p;
 p_2.tspan = [p_1.tspan(2) p_1.tspan(2)+18*60];
 
-%% Simulate
+% Simulate
 
 % Previous simulation.
 [t,x] = ode15s(@(t,x) m.ode(t,x,p_0),p_0.tspan,m.x0,opt);
@@ -55,124 +91,4 @@ out_2 = m.simout2struct(t,x,p_2);
 out = concatStruct(out_1, out_2);
 
 out.t = out.t/60;
-
-
-%% Plot
-
-fontSize = 8;
-axisSize = 6;
-
-% f = figure('Position',[0,0,235,225]);
-set(gca,'Fontsize',axisSize);
-
-
-yyaxis left;
-hold on;
-ylabel('Protein $x$ $\; [$fg$]$','interpreter','latex','FontSize',fontSize);
-ylim([0 100]);
-% yticks([0:0.25:2]);
-plot(out.t, out.cell__x__m);
-
-yyaxis right;
-hold on;
-ylabel('Growth rate $\; [$min$^{-1}]$','interpreter','latex','FontSize',fontSize);
-ylim([0 0.03]);
-plot(out.t, out.cell__mu);
-xlabel('Time $\; [h]$','interpreter','latex','FontSize',fontSize);
-
-% title('\textbf{Biorreactor model}','interpreter','latex','FontSize',fontSize);
-
-
-% Plot result.
-% figure(1);
-% 
-% subplot(2,2,1);
-% 
-% hold on;
-% plot(out.t, out.cell__x__m);
-% plot(out.t, out.cell__ref);
-% 
-% grid on;
-% title('Output');
-% legend('x');
-% ylim([0 +inf])
-% 
-% subplot(2,2,2);
-% 
-% hold on;
-% plot(out.t, out.cell__mu);
-% 
-% grid on;
-% title('Growth rate');
-% legend('\mu');
-% ylim([0 +inf])
-% 
-% subplot(2,2,3);
-% 
-% hold on;
-% plot(out.t, out.cell__z1__m);
-% plot(out.t, out.cell__z2__m);
-% 
-% grid on;
-% title('Control action');
-% legend('z1','z2');
-% ylim([0 +inf])
-% 
-% subplot(2,2,4);
-% 
-% hold on;
-% plot(out.t, out.cell__p_r__m);
-% plot(out.t, out.cell__p_nr__m)
-% plot(out.t, out.cell__m_h);
-% plot(out.t, out.cell__m_p);
-% 
-% grid on;
-% title('ribo and non-ribo');
-% legend('r','nr');
-% ylim([0 +inf])
-
-
-
-% figure(2);
-% 
-% controller = out.cell__z1__m + out.cell__z2__m + out.cell__z12__m; 
-% 
-% mass = [
-%     out.cell__x__m controller out.cell__p_r__m out.cell__p_nr__m 
-%     ];
-% 
-% subplot(3,1,1);
-% 
-% area(out.t, mass);
-% ylim([0 +inf])
-% legend('x','controller','ribo','ron-ribo');
-% 
-% subplot(3,1,2);
-% 
-% area(out.t, mass./sum(mass')');
-% ylim([0 +inf])
-% legend('x','controller','ribo','ron-ribo');
-% 
-% subplot(3,1,3);
-% 
-% hold on;
-% plot(out.t, (controller + out.cell__x__m)./sum(mass')'*100);
-% plot(out.t, 100-out.cell__mu./out.cell__mu(1)*100);
-% ylim([0 100]);
-% legend('heterologous', 'growth');
-
-
-
-% figure(3);
-% 
-% controller = out.cell__z1__J + out.cell__z2__J + out.cell__z12__J; 
-% 
-% mass = [
-%     out.cell__p_r__N.*out.cell__p_r__J out.cell__p_nr__N.*out.cell__p_nr__J out.cell__x__J controller
-%     ];
-% 
-% area(out.t, mass);
-% ylim([0 +inf]);
-% legend('ribo','ron-ribo','x','controller');
-
-
+end
